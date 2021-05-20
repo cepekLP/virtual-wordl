879bac212b0fa57bda2e8bc8bbf24c09513e1b97
Marcin Nadarzynski <m.nadarzynski01@gmail.com>
2021-05-20 21:18:04 +0200

    Add action for SosnowskyHogweed and Dandelion

--- plants/Dandelion.py
+++ plants/Dandelion.py
@@ -1,4 +1,6 @@
-from Plant import Plant
+import random
+
+from Plant import Plant, PLANT_DELAY, PLANT_CHANCE_TO_MULTIPLY
 from Point import Point
 from World import World
 
@@ -6,3 +8,11 @@ from World import World
 class Dandelion(Plant):
     def __init__(self, position: Point, world_ref: World):
         super().__init__(0, position, world_ref)
+
+    def action(self) -> None:
+        for i in range(3):
+            if random.random() < PLANT_CHANCE_TO_MULTIPLY and self.delay == 0:
+                self.multiply()
+                self.delay = PLANT_DELAY
+            elif self.delay > 0:
+                self.delay = self.delay - 1
--- plants/SosnowskyHogweed.py
+++ plants/SosnowskyHogweed.py
@@ -1,4 +1,6 @@
-from Plant import Plant
+import random
+
+from Plant import Plant, PLANT_DELAY, PLANT_CHANCE_TO_MULTIPLY
 from Point import Point
 from World import World
 
@@ -6,3 +8,17 @@ from World import World
 class SosnowskyHogweed(Plant):
     def __init__(self, position: Point, world_ref: World):
         super().__init__(11, position, world_ref)
+
+    def action(self):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i != 0 and j != 0:
+                    organism = self.world.check_collision(Point(i, j))
+                    if organism is not None:
+                        self.world.remove_organism(organism)
+
+        if random.random() < PLANT_CHANCE_TO_MULTIPLY and self.delay == 0:
+            self.multiply()
+            self.delay = PLANT_DELAY
+        elif self.delay > 0:
+            self.delay = self.delay - 1
